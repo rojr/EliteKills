df253014fd5ba7a45dd6ff1b579fb58dc2197375
robmadeyou <robmadeyou@gmail.com>
2018-05-05 00:41:27 +0100

    Making things better ;)

--- renderer.js
+++ renderer.js
@@ -15,32 +15,63 @@ var players = document.querySelector("#playerList");
 const eliteSaveFiles = os.homedir() + "/Saved\ Games/Frontier\ Developments/Elite\ Dangerous/";
 
 var allEvents = [];
+
 var pvpKills = [];
+
+var uniqPvpKills = [];
 
 var lastFile = '';
 
-fs.readdir(eliteSaveFiles, (err, files) => {
-  files.forEach(file => {
-    if (!file.startsWith("Journal.")) {
-      return;
-    }
-    lastFile = eliteSaveFiles + file;
+var files = fs.readdirSync(eliteSaveFiles);
 
-    var lineReader = readline.createInterface({
-      input: fs.createReadStream(lastFile)
-    });
+files = files.sort();
 
-    lineReader.on('line', function (line) {
+var fileGotTo = 0;
+
+for (var i = 0; i < files.length; i++) {
+  file = files[i];
+
+  if (!file.startsWith("Journal.")) {
+    continue;
+  }
+
+  lastFile = eliteSaveFiles + file;
+
+  processFile(lastFile);
+}
+
+setInterval(function() {
+  processFile(lastFile);
+  console.log(lastFile);
+
+}, 1000);
+
+function processFile(file) {
+  var lineReader = readline.createInterface({
+    input: fs.createReadStream(lastFile)
+  });
+
+  lineReader.on('line', function (line) {
+    try {
       var obj = JSON.parse(line);
-      allEvents.push(obj);
 
       if (obj.event == "PVPKill") {
+        if (uniqPvpKills.count(line) == 0) {
+          uniqPvpKills.push(line);
+        } else {
+          return;
+        }
+
         pvpKills.push(obj.Victim);
 
         var event = document.createElement("p");
         event.innerHTML = pvpKills.count(obj.Victim) + ' x ' + obj.Victim;
         players.prepend(event);
       }
-    });
+    } catch (ex) {}
   });
-})
+
+  lineReader.on('close', function () {
+    
+  });
+}
